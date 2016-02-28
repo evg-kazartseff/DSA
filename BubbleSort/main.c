20c6041901cda6a1b73ddad70d1d3e7b192e8f2e
evg-kazartseff <evg.kazartseff@yandex.ru>
2016-02-28 23:05:36 +0600

    code style

--- BubbleSort/main.c
+++ BubbleSort/main.c
@@ -3,47 +3,47 @@
 #include <inttypes.h>
 #include "fanc.h"
 
-int main (int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		printf ("Missing size of array!\n");
-		return EXIT_FAILURE;
-	}
+    if (argc < 2) {
+	printf("Missing size of array!\n");
+	return EXIT_FAILURE;
+    }
 
-	int size_array = atoi(argv[1]);
-	int * array = (int *)malloc(size_array*sizeof(uint32_t));
+    int size_array = atoi(argv[1]);
+    int *array = (int *) malloc(size_array * sizeof(uint32_t));
 
-	for (int i = 0; i < size_array; i++) {
-		array[i] = getrand(0,100000);
+    for (int i = 0; i < size_array; i++) {
+	array[i] = getrand(0, 100000);
 //        printf ("array[%d]= %d ",i,array[i]);
+    }
+
+    double time = wtime();
+
+    for (int i = 0; i < size_array - 1; i++) {
+	// сравниваем два соседних элемента.
+	for (int j = 0; j < size_array - i - 1; j++) {
+	    if (array[j] > array[j + 1]) {
+		// если они идут в неправильном порядке, то
+		//  меняем их местами.
+		int tmp = array[j];
+		array[j] = array[j + 1];
+		array[j + 1] = tmp;
+	    }
 	}
-
-	double time = wtime();
-
-    	for(int i = 0 ; i < size_array - 1; i++) {
-        	// сравниваем два соседних элемента.
-        	for(int j = 0 ; j < size_array - i - 1 ; j++) {
-            		if(array[j] > array[j+1]) {
-                	// если они идут в неправильном порядке, то
-                	//  меняем их местами.
-                	int tmp = array[j];
-                	array[j] = array[j+1];
-                	array[j+1] = tmp;
-            		}
-        	}
-    	}
+    }
 
 
 //    for (int i = 0; i < size_array; i++) {
 //    printf ("array[%d]= %d ",i,array[i]);
-//	}
+//      }
 
-	time = wtime() - time;
+    time = wtime() - time;
 
-	FILE * tb;
-	tb = fopen ("bubblesort.dat", "a");
-	fprintf (tb,"%d %.6f\n",size_array, time);
+    FILE *tb;
+    tb = fopen("bubblesort.dat", "a");
+    fprintf(tb, "%d %.6f\n", size_array, time);
 
-	free(array);
-	return EXIT_SUCCESS;
+    free(array);
+    return EXIT_SUCCESS;
 }
--- CountingSort/main.c
+++ CountingSort/main.c
@@ -3,58 +3,60 @@
 #include <inttypes.h>
 #include "fanc.h"
 
-int main (int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		printf ("Missing size of array!\n");
-		return EXIT_FAILURE;
-	}
+    if (argc < 2) {
+	printf("Missing size of array!\n");
+	return EXIT_FAILURE;
+    }
 
-	int size_array = atoi(argv[1]);
-	int * array = (int *)malloc(size_array*sizeof(uint32_t));
-	int max, min;
-	int i,z,j;
+    int size_array = atoi(argv[1]);
+    int *array = (int *) malloc(size_array * sizeof(uint32_t));
+    int max, min;
+    int i, z, j;
 
-	for (i = 0; i < size_array; i++) {
-		array[i] = getrand(0,100000);
+    for (i = 0; i < size_array; i++) {
+	array[i] = getrand(0, 100000);
 //        printf ("array[%d]= %d ",i,array[i]);
+    }
+
+    min = max = array[0];
+    for (i = 1; i < size_array; i++) {
+	if (array[i] < min) {
+	    min = array[i];
+	} else if (array[i] > max) {
+	    max = array[i];
 	}
+    }
 
-	min = max = array[0];
-  	for(i=1; i < size_array; i++) {
-    	if ( array[i] < min ) {
-      		min = array[i];
-    	}
-		else if ( array[i] > max ) {
-      		max = array[i];
-    	}
-  	}
-
-	double time = wtime();
-	int range = max - min + 1;
-  	int *swap = malloc(range * sizeof(*array));
+    double time = wtime();
+    int range = max - min + 1;
+    int *swap = malloc(range * sizeof(*array));
 
 
-	for(i = 0; i < range; i++) swap[i] = 0;
-	for(i = 0; i < size_array; i++) swap[ array[i] - min ]++;
+    for (i = 0; i < range; i++)
+	swap[i] = 0;
+    for (i = 0; i < size_array; i++)
+	swap[array[i] - min]++;
 
-	for(i = min, z = 0; i <= max; i++) {
-    	for(j = 0; j < swap[i - min]; j++) {
-      		array[z++] = i;
-    	}
-  	}
-	time = wtime() - time;
+    for (i = min, z = 0; i <= max; i++) {
+	for (j = 0; j < swap[i - min]; j++) {
+	    array[z++] = i;
+	}
+    }
+    time = wtime() - time;
 
-	FILE * tb;
-	tb = fopen ("countingsort.dat", "a");
-	fprintf (tb,"%d %.6f\n",size_array, time);
+    FILE *tb;
+    tb = fopen("countingsort.dat", "a");
+    fprintf(tb, "%d %.6f\n", size_array, time);
 
-//   	for (i = 0; i < size_array; i++) {
+//      for (i = 0; i < size_array; i++) {
 //    printf ("array[%d]= %d ",i,array[i]);
-//	}
+//      }
 
 
-	free(array);
+    free(array);
+    free(swap);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
