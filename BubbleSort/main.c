083d3b8fe2541b74d66b7fb92a17c658a89ed131
evg-kazartseff <evg.kazartseff@yandex.ru>
2016-03-13 13:09:27 +0600

    кодстайл & устойчивый counting

--- BubbleSort/main.c
+++ BubbleSort/main.c
@@ -6,37 +6,33 @@
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
-	printf("Missing size of array!\n");
-	return EXIT_FAILURE;
+        printf("Missing size of array!\n");
+        return EXIT_FAILURE;
     }
 
     int size_array = atoi(argv[1]);
     int *array = (int *) malloc(size_array * sizeof(uint32_t));
 
     for (int i = 0; i < size_array; i++) {
-	array[i] = getrand(0, 100000);
-//        printf ("array[%d]= %d ",i,array[i]);
+        array[i] = getrand(0, 100000);
+//      printf ("array[%d]= %d ",i,array[i]);
     }
 
     double time = wtime();
 
     for (int i = 0; i < size_array - 1; i++) {
-	// сравниваем два соседних элемента.
-	for (int j = 0; j < size_array - i - 1; j++) {
-	    if (array[j] > array[j + 1]) {
-		// если они идут в неправильном порядке, то
-		//  меняем их местами.
-		int tmp = array[j];
-		array[j] = array[j + 1];
-		array[j + 1] = tmp;
-	    }
-	}
+        for (int j = 0; j < size_array - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
     }
 
-
 //    for (int i = 0; i < size_array; i++) {
-//    printf ("array[%d]= %d ",i,array[i]);
-//      }
+//      printf ("array[%d]= %d ",i,array[i]);
+//    }
 
     time = wtime() - time;
 
--- CountingSort/main.c
+++ CountingSort/main.c
@@ -6,44 +6,57 @@
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
-	printf("Missing size of array!\n");
-	return EXIT_FAILURE;
+        printf("Missing size of array!\n");
+        return EXIT_FAILURE;
     }
 
     int size_array = atoi(argv[1]);
     int *array = (int *) malloc(size_array * sizeof(uint32_t));
     int max, min;
-    int i, z, j;
+    int i;
 
     for (i = 0; i < size_array; i++) {
-	array[i] = getrand(0, 100000);
-//        printf ("array[%d]= %d ",i,array[i]);
+        array[i] = getrand(0, 100000);
+//      printf ("array[%d]= %d ",i,array[i]);
     }
 
     min = max = array[0];
     for (i = 1; i < size_array; i++) {
-	if (array[i] < min) {
-	    min = array[i];
-	} else if (array[i] > max) {
-	    max = array[i];
-	}
+        if (array[i] < min) {
+            min = array[i];
+        } else if (array[i] > max) {
+            max = array[i];
+        }
     }
+
+    int range = max - min + 1;
 
     double time = wtime();
-    int range = max - min + 1;
-    int *swap = malloc(range * sizeof(*array));
+    int *counter_array = (int *) malloc(sizeof(int) * (range + 1));
+    int *temp_array = (int *) malloc(sizeof(int) * size_array);
 
+    for (i = 0; i < range + 1; i++)
+        counter_array[i] = 0;
 
-    for (i = 0; i < range; i++)
-	swap[i] = 0;
-    for (i = 0; i < size_array; i++)
-	swap[array[i] - min]++;
-
-    for (i = min, z = 0; i <= max; i++) {
-	for (j = 0; j < swap[i - min]; j++) {
-	    array[z++] = i;
-	}
+    for (i = 0; i < size_array; i++) {
+        int c = array[i] + 1 - min;
+        counter_array[c]++;
     }
+
+    for (i = 1; i < range; i++)
+        counter_array[i] += counter_array[i - 1];
+
+    for (i = (size_array - 1); i >= 0; i--) {
+        int c = array[i] - min;
+        int s = counter_array[c];
+        temp_array[s] = array[i];
+        counter_array[c]++;
+    }
+
+    for (i = 0; i < size_array; i++)
+        array[i] = temp_array[i];
+
+
     time = wtime() - time;
 
     FILE *tb;
@@ -51,12 +64,11 @@ int main(int argc, char *argv[])
     fprintf(tb, "%d %.6f\n", size_array, time);
 
 //      for (i = 0; i < size_array; i++) {
-//    printf ("array[%d]= %d ",i,array[i]);
+//              printf ("array[%d]= %d ",i,array[i]);
 //      }
 
-
-    free(array);
-    free(swap);
+    free(counter_array);
+    free(temp_array);
 
     return EXIT_SUCCESS;
 }
