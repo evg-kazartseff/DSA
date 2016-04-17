a8ce6e400c3d5bb8757e71685c5a3b7d875556b6
evg-kazartseff <evg.kazartseff@yandex.ru>
2016-04-17 17:43:21 +0600

    test3

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -4,8 +4,11 @@
 #include <stdio.h>
 #include <time.h>
 #include "fanc.h"
+#include <unistd.h> 
 
 #define dictionary 570000
+#define exp 2
+#define HASHTAB_SIZE 128
 
 int main(int argc, char **argv)
 {
@@ -17,11 +20,10 @@ int main(int argc, char **argv)
 	int size = atoi(argv[1]);
 	
 	FILE *input = fopen("../dictionary/input.dat", "r");
-	if (input == NULL)
-    {
-        printf("Can't open input file!\n");
-        return EXIT_FAILURE;
-    }
+	if (input == NULL) {
+	    printf("Can't open input file!\n");
+	    return EXIT_FAILURE;
+	}
 	
 	
 	int i = 0;
@@ -43,80 +45,143 @@ int main(int argc, char **argv)
 		//printf ("buf(%d): %s\n", i+1, buf[i]);
 	}
 	
-	srand(time(NULL));
-	int rand_i = getrand(0,size);
-    printf ("rand_word: %s idx: %d\n",buf[rand_i], rand_i+1);
-    
-    //построение дерева    
-    struct bstree *tree ,*node;
+	//построение дерева    
+	struct bstree *tree ,*node;
 	tree = bstree_create(buf[0],1);
 	for (i = 2; i <= size; i++) {
 		bstree_add(tree,buf[i-1],i);
 	}
-	int hash_mode = 0;
+	
 	//создание таблицы
-	struct listnode *hash;
+	int hash_mode = 0;
+	struct listnode *hash, *hash_node;
 	hashtab_init(hashtab);
 	for (i = 1; i <= size; i++) {
 		hashtab_add(hashtab,buf[i-1],i,hash_mode);
 	}
-
+	
+	int coliz = 0;
+	for (i = 0; i < HASHTAB_SIZE; i++) {
+	    if (hashtab[i] != NULL) {
+		hash_node=hashtab[i];
+		if (hash_node->next != NULL) {
+		    //printf("hashtab %d %s\n",i, hash_node->key);
+		    coliz++;
+		}
+	    }
+	}
+	printf ("coliz(KPHash) %d\n",coliz);
+	
+	int count;
 	//test 1 lookup--
-	printf("lookup\n");
-	double time = wtime();
-	node = bstree_lookup(tree,buf[rand_i]);
-	time = wtime() - time;
-	FILE *tb1;
-    tb1 = fopen("../test/bstree_lookup.dat", "a");
-    fprintf(tb1, "%d %.8f\n", size, time);
-	if (node != NULL) {
-		printf ("BSTree: %s, %d %.8f\n",node->key,node->value, time);
+	double lookup_time_bstree = 0;
+	double lookup_time_hashtab = 0;
+	double time_exp;
+	int rand_i;
+	for (count = 1; count <= exp; count++) {
+	    sleep(1);
+	    printf("lookup exp № %d\n",count);
+	    printf("size %d\n",size);
+	    
+	    srand(time(NULL));
+	    rand_i = getrand(0,size);
+	    printf ("rand_word: %s idx: %d\n",buf[rand_i], rand_i+1);	
+	    
+	    time_exp = wtime();
+	    node = bstree_lookup(tree,buf[rand_i]);
+	    time_exp = wtime() - time_exp;
+	    
+	    lookup_time_bstree = lookup_time_bstree + time_exp;
+	    if (node != NULL) {
+		printf ("BSTree: %s, %d %.8f\n",node->key,node->value, time_exp);
+	    }
+	    
+	    time_exp = wtime();
+	    hash = hashtab_lookup(hashtab,buf[rand_i],hash_mode);
+	    time_exp = wtime() - time_exp;
+	    
+	    lookup_time_hashtab = lookup_time_hashtab + time_exp;
+	    printf("HashTab (KPHash): %s, %d %.8f\n",hash->key, hash->value, time_exp);
 	}
 	
-	time = wtime();
-	hash = hashtab_lookup(hashtab,buf[rand_i],hash_mode);
-	time = wtime() - time;
+	lookup_time_bstree = lookup_time_bstree/exp;
+	lookup_time_hashtab = lookup_time_hashtab/exp;
+	
+	FILE *tb1;
+	tb1 = fopen("../test/bstree_lookup.dat", "a");
+	fprintf(tb1, "%d %.8f\n", size, lookup_time_bstree);
+
 	FILE *tb2;
-    tb2 = fopen("../test/hashtab_lookup_KPHash.dat", "a");
-    fprintf(tb2, "%d %.8f\n", size, time);
-	printf("HashTab (KPHash): %s, %d %.8f\n",hash->key, hash->value, time);
+	tb2 = fopen("../test/hashtab_lookup_KPHash.dat", "a");
+	fprintf(tb2, "%d %.8f %d\n", size, lookup_time_hashtab, coliz);
 	//--
 	
 	//test2 add--
 	printf("add\n");
-	time = wtime();
+	time_exp = wtime();
 	bstree_add(tree,buf[size],size+1);
-	time = wtime() - time;
+	time_exp = wtime() - time_exp;
+	printf("bstree_add %s %d\n",buf[size],size+1);
+	    
 	FILE *tb3;
-    tb3 = fopen("../test/bstree_add.dat", "a");
-    fprintf(tb3, "%d %.8f\n", size, time);
-    printf ("BSTree: %s, %d %.8f\n",buf[size],size+1, time);
-    
-    time = wtime();
-    hashtab_add(hashtab,buf[size],size+1,hash_mode);
-    time = wtime() - time;
-    FILE *tb4;
-    tb4 = fopen("../test/hashtab_add.dat", "a");
-    fprintf(tb4, "%d %.8f\n", size, time);
-    printf("HashTab (KPHash): %s, %d %.8f\n",buf[size], size+1, time);
+	tb3 = fopen("../test/bstree_add.dat", "a");
+	fprintf(tb3, "%d %.8f\n", size, time_exp);
+	printf ("BSTree: %s, %d %.8f\n",buf[size],size+1, time_exp);	    
+	
+	time_exp = wtime();
+	hashtab_add(hashtab,buf[size],size+1,hash_mode);
+	time_exp = wtime() - time_exp;
+	printf("hashtab(KPHash)_add %s %d\n",buf[size],size+1);
+
+	FILE *tb4;
+	tb4 = fopen("../test/hashtab_add.dat", "a");
+	fprintf(tb4, "%d %.8f\n", size, time_exp);
+	printf("HashTab (KPHash): %s, %d %.8f\n",buf[size], size+1, time_exp);
 	//--
 	
 	//test3 hash_mode lookup--
 	printf("lookup\n");
+	double lookup_time_hashtab_ELFHash = 0;
 	hash_mode = 1;
 	struct listnode *hash_tb;
 	hashtab_init(hashtab);
+	
 	for (i = 1; i <= size; i++) {
 		hashtab_add(hashtab,buf[i-1],i,hash_mode);
 	}
-	time = wtime();
-	hash_tb = hashtab_lookup(hashtab,buf[rand_i],hash_mode);
-	time = wtime() - time;
-	FILE *tb5;
-    tb5 = fopen("../test/hashtab_lookup_ELFHash.dat", "a");
-    fprintf(tb5, "%d %.8f\n", size, time);
-	printf("HashTab (ELFHash): %s, %d %.8f\n",hash_tb->key, hash_tb->value, time);
-	//--
 	
+	coliz = 0;
+	for (i = 0; i < HASHTAB_SIZE; i++) {
+	    if (hashtab[i] != NULL) {
+		hash_node=hashtab[i];
+		if (hash_node->next != NULL) {
+		    //printf("hashtab %d %s\n",i, hash_node->key);
+		    coliz++;
+		}
+	    }
+	}
+	printf ("coliz(ELFHash) %d\n",coliz);
+	
+	for (count = 1; count <= exp; count++) {
+	    sleep(1);
+	    printf("lookup exp № %d\n",count);
+	    printf("size %d\n",size);
+	    
+	    srand(time(NULL));
+	    rand_i = getrand(0,size);
+	    printf ("rand_word: %s idx: %d\n",buf[rand_i], rand_i+1);
+	    
+	    time_exp = wtime();
+	    hash_tb = hashtab_lookup(hashtab,buf[rand_i],hash_mode);
+	    time_exp = wtime() - time_exp;
+	    printf("HashTab (ELFHash): %s, %d %.8f\n",hash_tb->key, hash_tb->value, time_exp);
+	    lookup_time_hashtab_ELFHash = lookup_time_hashtab_ELFHash + time_exp;
+	}
+	lookup_time_hashtab_ELFHash = lookup_time_hashtab_ELFHash/exp;
+	
+	FILE *tb5;
+	tb5 = fopen("../test/hashtab_lookup_ELFHash.dat", "a");
+	fprintf(tb5, "%d %.8f %d\n", size, lookup_time_hashtab_ELFHash, coliz);
+	//--
 	return 0;
 }
---
+++ lab2/test/mkgraph.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/gnuplot
+set terminal png size 800,480 enhanced font 'Arial, 16'
+
+
+set style line 1 linecolor rgb 'red' linetype 1 linewidth 2
+set style line 2 linecolor rgb 'green' linetype 1 linewidth 2
+
+set border linewidth 1
+set key top left
+set grid
+set mytics 0
+set format y "%.6f"
+set xlabel "Number of elements" font "Arial, 16"
+set format x "%.0f"
+set ylabel "Execution time (sec)" font "Arial, 16"
+set xtics 100000 font "Arial, 12"
+set ytics font "Arial, 12"
+set rmargin 4
+set tmargin 2
+set mxtics
+
+set output 'lookup.png'
+plot "./bstree_lookup.dat" using 1:2 title "bstree" with linespoints ls 1,\
+     "./hashtab_lookup_KPHash.dat" using 1:2 title "hashtab" with linespoints ls 2,\
+
+set output 'lookup_bstree.png'
+plot "./bstree_lookup.dat" using 1:2 title "bstree" with linespoints ls 1
+
+set output 'lookup_KPHash.png'
+plot "./hashtab_lookup_KPHash.dat" using 1:2 title "KPHash_lookup" with linespoints ls 2
+
+set output 'bstree_add.png'
+plot "./bstree_add.dat" using 1:2 title "bstree_add" with linespoints ls 1
+
+set output 'hashtab_add.png'
+plot "./hashtab_add.dat" using 1:2 title "hashtab_add" with linespoints ls 2
+
+set output 'lookup_ELFHash.png'
+plot "./hashtab_lookup_ELFHash.dat" using 1:2 title "ELFHash_lookup" with linespoints ls 2
+
+set output 'lookup_hashtab.png'
+plot "./hashtab_lookup_ELFHash.dat" using 1:2 title "lookup_ELFHash" with linespoints ls 1,\
+     "./hashtab_lookup_KPHash.dat" using 1:2 title "lookup_KPHash" with linespoints ls 2,\
