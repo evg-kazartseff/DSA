3025f7d7e608c1ac75d99e94b054a433cf49010e
evg-kazartseff <evg.kazartseff@yandex.ru>
2016-04-21 15:15:23 +0600

    codestyle

--- lab2/src/hashtab.h
+++ lab2/src/hashtab.h
@@ -5,13 +5,13 @@
 #include <string.h>
 #include <stdio.h>
 
-#define HASHTAB_SIZE 128
+#define HASHTAB_SIZE 2047
 #define HASHTAB_MUL 31
 
 struct listnode {
-	char *key;
-	int value;
-	struct listnode *next;
+    char *key;
+    int value;
+    struct listnode *next;
 };
 struct listnode *hashtab[HASHTAB_SIZE];
 
@@ -19,7 +19,8 @@ unsigned int hashtab_hash(char *key);
 unsigned int ELFHash(char *key);
 void hashtab_init(struct listnode **hashtab);
 void hashtab_add(struct listnode **hashtab, char *key, int value, int f_hash);
-struct listnode *hashtab_lookup(struct listnode **hashtab, char *key, int f_hash);
+struct listnode *hashtab_lookup(struct listnode **hashtab, char *key,
+                                int f_hash);
 void hashtab_delete(struct listnode **hashtab, char *key, int f_hash);
 
 #endif
